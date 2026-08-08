b94d2bc04faefa6a27733e497b223ca7a5b57b9b
Shreyanka11 <shreyankas11@gmail.com>
2026-08-08 21:22:32 +0530

    Update Hello World message

--- Java/Basics/HellloWorld.java
+++ Java/Basics/HellloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String [] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Day 1 of my 60 day placement journey");
     }
 }
